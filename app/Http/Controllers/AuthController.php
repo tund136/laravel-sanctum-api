739b77cdb8e93cd55999319165f080169775bf29
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-24 16:51:35 +0700

    Login User & get Token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,6 +31,32 @@ class AuthController extends Controller {
         return response($response, 201);
     }
 
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check password
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
     public function logout(Response $request) {
         auth()->user()->tokens()->delete();
 
