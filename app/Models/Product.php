05f9c49a82e5b9744113b53cb7d1bb1ab26685d4
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-24 14:49:45 +0700

    Using the Model

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static create()
+ */
+class Product extends Model {
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price'
+    ];
+}
